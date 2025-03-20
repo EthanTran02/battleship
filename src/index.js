02d329ccc8671e80b10834d1b26ffa513018a336
Ethan Tran <khoitranec@gmail.com>
2025-03-20 16:58:35 +0700

    Small refactor code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import './style.css';
 import { Player } from './modules/player';
 import { Ship } from './modules/ship';
-import { createBoard, updateBoard } from './modules/boardDOM';
+import { updateBoard } from './modules/boardDOM';
 
-createBoard('board-container1');
-createBoard('board-container2');
+// createBoard('board-container1');
+// createBoard('board-container2');
 
 const realPlayer = new Player('real');
-const computerPlayer = new Player('computer');
-
 const ship = new Ship(3);
+const ship2 = new Ship(5);
 realPlayer.gameboard.placeShip(ship, [0, 0]);
-console.log(realPlayer.gameboard.board);
+realPlayer.gameboard.placeShip(ship2, [4, 5]);
 updateBoard(realPlayer, 'board-container1');
+
+const computerPlayer = new Player('computer');
+const compShip = new Ship(2);
+const compShip2 = new Ship(4);
+computerPlayer.gameboard.placeShip(compShip, [5, 2]);
+computerPlayer.gameboard.placeShip(compShip2, [2, 7]);
+updateBoard(computerPlayer, 'board-container2');
--- src/modules/boardDOM.js
+++ src/modules/boardDOM.js
@@ -1,22 +1,22 @@
-export function createBoard(containerID) {
-  const container = document.getElementById(containerID);
-  if (!container) {
-    console.error(`Container ${containerID} not found`);
-    return;
-  }
-  const grid = document.createElement('div');
-  grid.classList.add('grid');
-  grid.id = `${containerID}-grid`;
+// export function createBoard(containerID) {
+//   const container = document.getElementById(containerID);
+//   if (!container) {
+//     throw new Error(`Container ${containerID} not found`);
+//     return;
+//   }
+//   const grid = document.createElement('div');
+//   grid.classList.add('grid');
+//   grid.id = `${containerID}-grid`;
 
-  for (let i = 0; i < 100; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.dataset.x = i % 10;
-    cell.dataset.y = Math.floor(i / 10);
-    grid.appendChild(cell);
-  }
-  container.appendChild(grid);
-}
+//   for (let i = 0; i < 100; i++) {
+//     const cell = document.createElement('div');
+//     cell.classList.add('cell');
+//     cell.dataset.x = i % 10;
+//     cell.dataset.y = Math.floor(i / 10);
+//     grid.appendChild(cell);
+//   }
+//   container.appendChild(grid);
+// }
 
 export function updateBoard(player, containerID) {
   const container = document.getElementById(containerID);
